ef3a6fe1d8e6c2f69d89964e2dd571bba49087b9
ShumailaKhan12 <shumaila1641@gmail.com>
2025-07-14 15:47:07 +0530

    New

--- src/pages/MyReferral/MyReferralScreen.jsx
+++ src/pages/MyReferral/MyReferralScreen.jsx
@@ -21,6 +21,7 @@ import { postData } from '../../services/api';
 import { DecryptFunction } from '../../utils/decryptFunction';
 import { UserContext } from '../../utils/UseContext/useContext';
 import { NavLink } from 'react-router-dom';
+import { toastInfo } from '../../utils/toster';
 
 // Import Json
 const faqData = [
@@ -261,14 +262,16 @@ const MyReferralScreen = () => {
                   </div>
                   <div className="mt-3 row justify-content-between justify-content-lg-center align-items-center">
                     <div className='col-6 col-lg-3'>
-                     <NavLink to={"/invite-friend"}>
+                     <NavLink to={"/invitefriend"}>
                        <button className="py-2 mb-0 w-100 rounded-3 text-white bg-transparent border border-white font-16 montserrat-semibold">
                         Invite a Friend
                       </button>
                      </NavLink>
                     </div>
                     <div className='col-6 col-lg-3'>
-                      <button className="py-2 w-100 rounded-3 border-0 bg-white text-blue font-16 montserrat-semibold">
+                      <button className="py-2 w-100 rounded-3 border-0 bg-white text-blue font-16 montserrat-semibold"
+                      onClick={()=>toastInfo("Comming Soon")}
+                      >
                         Track
                       </button>
                     </div>
