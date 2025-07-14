cab37eadb7970acaf705f0caddedf199d0e88b7b
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2025-07-14 15:44:28 +0530

    corrections

--- src/pages/MyReferral/MyReferralScreen.jsx
+++ src/pages/MyReferral/MyReferralScreen.jsx
@@ -110,6 +110,7 @@ const MyReferralScreen = () => {
         mode: Auth?.mode,
       });
       const Decrpty = await DecryptFunction(enyptData);
+      console.log('Decrpty: ', Decrpty);
       setRefralDataAPI(Decrpty);
     } catch (error) {
       console.log('error: ', error);
