988b6693c51e6af49109cec85010ecb96ba134d6
ShumailaKhan12 <shumaila1641@gmail.com>
2025-07-14 17:03:02 +0530

    New Changes

--- src/pages/MyReferral/referralCards.jsx
+++ src/pages/MyReferral/referralCards.jsx
@@ -271,9 +271,9 @@ const ReferralCards = ({ RefralDataAPI }) => {
       <p className="font-size-32 referral-txt montserrat-bold text-white pt-4">{card.count}</p>
       <p className="font-size-28 referral-txt montserrat-semibold text-white mb-0">{card.title}</p>
       <p className="font-size-14 montserrat-semibold referral-yellow-text">{card.subtitle}</p>
-      <button className="border-0 text-white rounded background-text-blue px-4 py-2 mt-3">
+      {/* <button className="border-0 text-white rounded background-text-blue px-4 py-2 mt-3">
         View
-      </button>
+      </button> */}
     </div>
   );
 
